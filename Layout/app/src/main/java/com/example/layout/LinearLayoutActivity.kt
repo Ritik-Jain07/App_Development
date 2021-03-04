3dd29928d030ec3d4fd1246078ccfaaf0cc62206
Ritik_Jain <ritik.jain@tothenew.com>
2021-03-04 17:57:57 +0530

    Layout Project

---
+++ Layout/app/src/main/java/com/example/layout/LinearLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout)
+    }
+}
